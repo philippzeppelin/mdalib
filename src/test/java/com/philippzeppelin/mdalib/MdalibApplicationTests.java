62eb00f7ab7ae8c47dbdadff4c5c3ba4e5561cb1
philippzeppelin <philippzeppelin@gmail.com>
2025-02-20 19:35:39 +0300

    Initial commit

---
+++ src/test/java/com/philippzeppelin/mdalib/MdalibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.philippzeppelin.mdalib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdalibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
