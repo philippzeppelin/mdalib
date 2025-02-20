26534119247b903a6daf2408cf97d791cdb0bf11
philippzeppelin <philippzeppelin@gmail.com>
2025-02-20 23:47:38 +0300

    Added AuthorRepository, AvailabilityRepository, BookRepository

--- src/main/java/com/philippzeppelin/mdalib/ApplicationRunner.java
+++ src/main/java/com/philippzeppelin/mdalib/ApplicationRunner.java
@@ -1,28 +1,43 @@
 package com.philippzeppelin.mdalib;
 
+import com.philippzeppelin.mdalib.repository.AuthorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@RequiredArgsConstructor
 public class ApplicationRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApplicationRunner.class, args);
-		String url = "jdbc:postgresql://localhost:5433/postgres";
-		String user = "postgres";
-		String password = "pass";
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRunner.class, args);
+    }
 
-		try (Connection connection = DriverManager.getConnection(url, user, password)) {
-			System.out.println("Connection successful!");
-		} catch (Exception e) {
-			System.out.println("Connection is not successful!");
-			e.printStackTrace();
-		}
-	}
+    @Bean
+    public CommandLineRunner commandLineRunner(AuthorRepository authorRepository) {
+        return args -> {
+            testConnection();
+        };
+    }
 
+    private static void testConnection() {
+        String url = "jdbc:postgresql://localhost:5433/postgres";
+        String user = "postgres";
+        String password = "pass";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Connection successful!");
+        } catch (Exception e) {
+            System.out.println("Connection is not successful!");
+            e.printStackTrace();
+        }
+    }
 }
