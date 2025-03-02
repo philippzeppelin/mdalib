0c85b0dd99e67356a2bf62fca2ea691431162690
philippzeppelin <philippzeppelin@gmail.com>
2025-03-02 19:30:02 +0300

    Created tests for AuthorServiceImpl

---
+++ src/test/java/com/philippzeppelin/mdalib/service/serviceImpl/BookServiceImplTest.java
@@ -0,0 +1,16 @@
+package com.philippzeppelin.mdalib.service.serviceImpl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookServiceImplTest {
+
+    @Test
+    void addBook() {
+    }
+
+    @Test
+    void deleteBook() {
+    }
+}
